d40359a6b8742e15003099644d396c8c1c7a39a0
stephen-klinck <klinck.steve.h@gmail.com>
2019-07-14 14:02:35 -0400

    added 'currently' to calculating message

--- bikeshare.py
+++ bikeshare.py
@@ -96,7 +96,7 @@ def time_stats(df):
     common_month_dict = {1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June'}
     common_hour_dict = {0:'12 am', 1: '1 am', 2:'2 am', 3:'3 am', 4:'4 am', 5:'5 am', 6:'6 am', 7:'7 am', 8:'8 am', 9:'9 am', 10:'10 am', 11:'11 am', 12:'12 pm', 13:'1 pm', 14:'2 pm', 15:'3 pm', 16:'4 pm', 17:'5 pm', 18:'6 pm', 19:'7 pm', 20:'8 pm', 21:'9 pm', 22:'10 pm', 23:'11 pm'}
 
-    print('\nCalculating The Most Frequent Times of Travel...\n')
+    print('\nCurrently Calculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
     # display the most common month
@@ -119,7 +119,7 @@ def time_stats(df):
 def station_stats(df):
     """Displays statistics on the most popular stations and trip."""
 
-    print('\nCalculating The Most Popular Stations and Trip...\n')
+    print('\nCurrently Calculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
 
     # display most commonly used start station
@@ -144,7 +144,7 @@ def station_stats(df):
 def trip_duration_stats(df):
     """Displays statistics on the total and average trip duration."""
 
-    print('\nCalculating Trip Duration...\n')
+    print('\nCurrently Calculating Trip Duration...\n')
     start_time = time.time()
 
     # display total travel time
@@ -162,7 +162,7 @@ def trip_duration_stats(df):
 def user_stats(df):
     """Displays statistics on bikeshare users."""
 
-    print('\nCalculating User Stats...\n')
+    print('\nCurrently Calculating User Stats...\n')
     start_time = time.time()
 
     # Display counts of user types
